14d102d1f125370503aa956dae12314a8f5a4487
blueVegetable <2601042086@qq.com>
2018-08-13 00:28:01 +0800

    增加了查看商品详情的界面

--- js/url.js
+++ js/url.js
@@ -1,3 +1,4 @@
 // var remoteURLTrue="http://111.230.97.64:8080";
 // var remoteURL="http://114.67.130.29:8080/manager";
 var remoteURL="http://111.230.97.64:8080";
+// var remoteURL="http://localhost:8080";

bd251c45e43a3fed74944d428ebd6f13422cec7f
blueVegetable <2601042086@qq.com>
2018-08-20 23:16:41 +0800

    添加了商品增加的类别功能

--- js/function.js
+++ js/function.js
@@ -1,13 +1,15 @@
-function getCityIdFromUrl(href) {
-    var parameterString=href.split("?")[1];
-    if(parameterString==null)
-        return "";
-    var parameters=parameterString.split("&");
-    for(var i=parameters.length-1;i>=0;i--) {
-        if(parameters[i].split("=")[0]=="cityId")
-            return parameters[i].split("=")[1];
-    }
-    return "";
+function getCity(cityId) {
+    var city="";
+    $.ajax({
+        type: "POST",
+        url:  remoteURL+"/Area/selectById",
+        data: {cityId:cityId},
+        async: false,
+        success: function(data){
+            cityId=data;
+        }
+    });
+    return city;
 }
 function getParameter(href,key) {
     var parameterString=href.split("?")[1];
